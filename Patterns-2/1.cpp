82552c959396a0caa8772898610b1cdf63a69dc2
Neeraj Gupta <gneeraj32595@gmail.com>
2021-07-16 13:00:21 +0000

    space pattern added

---
+++ Patterns-2/1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  int i = 1, j = 1;
+  while(i <= n) {
+    j=1;
+    while(j <= n-i) {
+      cout << " ";
+      j++;
+    }
+    while(j <= n) {
+      cout << "*";
+      j++;
+    }
+    cout << endl;
+    i++;
+  }
+}
