68a4148863315ff013f4b9c7382bcbc72179c0bb
Neeraj Gupta <gneeraj32595@gmail.com>
2021-08-08 21:45:41 +0000

    added faulty keyboard.cpp

--- ST-1/FaultyKeyboard.cpp
+++ ST-1/FaultyKeyboard.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
+#include<cstring>
 using namespace std;
-
+// TODO: Still one test case failing 5/6(working)
 bool canType(string s1, string s2) {
     int i = 0, j = 0;
     bool type = false;
@@ -14,6 +15,15 @@ bool canType(string s1, string s2) {
             j++;
             type = false;
         }
+    }
+    if(s1[i] != '\0')
+        type = false;
+    while(s2[j] != '\0') {
+      if(s1[i-1] != s2[j]) {
+        type = false;
+        break;
+      }
+      j++;
     }
     return type;
 }
